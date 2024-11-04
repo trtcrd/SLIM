8f385df77e3fde411a10e243d558027400023b8f
adriantich <adriantich@gmail.com>
2024-11-04 12:48:45 +0100

    chopper running and also have to be used afer joker-creator

--- lib/bash_scripts/run_optics.sh
+++ lib/bash_scripts/run_optics.sh
@@ -31,7 +31,7 @@ fi
 # export paths
 export PATH=$PATH:/root/.local/bin/:
 # export paths to minimap2 and spoa
-export PATH=$PATH:/app/lib/msi/bin/:/app/lib/ASHURE/spoa/build/bin/
+export PATH=$PATH:/app/lib/msi/bin/:/app/lib/ASHURE/spoa/build/bin/:
 
 # export paths to ashure
 export PATH=$PATH/app/lib/ASHURE/src/:
@@ -45,10 +45,76 @@ mkdir -p ft_dir_tmp
 cp ${fasta_files} ft_dir_tmp/.
 
 mkdir -p concat_dir_tmp
-cat ft_dir_tmp/* >concat_dir_tmp/concatenated.fastq
-python3 /app/lib/ASHURE/src/ashure.py clst -i concat_dir_tmp/concatenated.fasta -o centers.csv -N ${clst_N} -cs ${clst_csize} -tm ${clst_th_m} -ts ${clst_th_s} -iter ${clst_N_iter} -r
+if [[ ${fasta_files} == *.f*q* ]]; then
+    cat ft_dir_tmp/* >concat_dir_tmp/concatenated.fastq
+    concatenated=${directory}/concat_dir_tmp/concatenated.fastq
+else
+    cat ft_dir_tmp/* >concat_dir_tmp/concatenated.fasta
+    concatenated=${directory}/concat_dir_tmp/concatenated.fasta
+fi
 
-python3 /app/lib/python_scripts/optics.py -dir ${directory} -fasta_path "ft_dir_tmp/*" -centers centers.csv -otu_table ${otutab} -fasta_out ${fasta_out} -sim_thr ${sim_thr} 
+config_file="config.json"
+
+# Create the configuration file using a heredoc
+cat <<EOF > "$config_file"
+{
+  "spoa_path": "/app/lib/ASHURE/spoa/build/bin/spoa",
+  "minimap2_path": "/app/lib/msi/bin/minimap2",
+  "low_mem": false,
+  "fastq": [],
+  "exclude": [],
+  "primer_file": "",
+  "db_file": "",
+  "workspace": "${directory}/workspace/",
+  "cons_file": "",
+  "pmatch_file": "pmatch.csv.gz",
+  "cin_file": "${concatenated}",
+  "cout_file": "${directory}/centers.csv",
+  "log_file": "ashure.log",
+  "config_file": "${directory}/${config_file}",
+  "prfg_fs": "${min_length}-${max_length}",
+  "prfg_config": "-k5 -w1 -s 20 -P",
+  "prfg_pmr_thresh": 10,
+  "fgs_config": "-k10 -w1",
+  "frag_folder": "./frags/",
+  "msa_config": "-n -15 -g -10 -l 0 -r 0",
+  "msa_folder": "./msa/",
+  "msa_metric": "AS",
+  "msa_thresh": 50,
+  "msa_batch_size": 100,
+  "msa_thread_lock": false,
+  "msa_gap_thresh": 4,
+  "msa_padding": 0,
+  "fpmr_config": "-k5 -w1 -s 20 -P",
+  "fpmr_thresh": 10,
+  "clst_init": "",
+  "clst_folder": "./clusters/",
+  "clst_min_k": 5,
+  "clst_csize": ${clst_csize},
+  "clst_th_m": ${clst_th_m},
+  "clst_th_s": ${clst_th_s},
+  "clst_N": ${clst_N},
+  "clst_N_iter": ${clst_N_iter},
+  "clst_iter_out": "clst",
+  "clst_track": false,
+  "clst_pw_config": "-k15 -w10 -p 0.9 -D",
+  "subcommand": "clst"
+}
+EOF
+
+# python3 /app/lib/ASHURE/src/ashure.py clst -i ${concatenated} -o centers.csv -N ${clst_N} -cs ${clst_csize} -tm ${clst_th_m} -ts ${clst_th_s} -iter ${clst_N_iter} -r
+
+python3 /app/lib/ASHURE/src/ashure.py run -c ${config_file} -r clst
+
+mkdir -p fasta_dir_tmp
+
+for file in ft_dir_tmp/*; do
+    /app/lib/vsearch/bin/vsearch --fastq_filter ${directory}/${file} --fastaout ${directory}/fasta_dir_tmp/$(basename ${file} | cut -f1 -d '.').fasta --threads 8 --fastq_qmax 60
+done
+
+sed -i "s/\t/;/g" ${directory}/fasta_dir_tmp/*
+
+python3 /app/lib/python_scripts/optics.py -dir ${directory} -fasta_path "fasta_dir_tmp/*" -centers centers.csv -otu_table ${otutab} -fasta_out ${fasta_out} -sim_thr ${sim_thr} 
 
 rm -r ft_dir_tmp concat_dir_tmp centers.csv ashure.log
 exit 0
