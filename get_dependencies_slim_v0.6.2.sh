c00aaf0a72733a3cba613277925945367302ffdd
adriantich <adriantich@gmail.com>
2024-11-06 13:30:26 +0100

    wget for curl

--- get_dependencies_slim_v0.6.2.sh
+++ get_dependencies_slim_v0.6.2.sh
@@ -206,7 +206,7 @@ fi
 if [ ! -d "miniforge3" ]; then
 	mkdir miniforge3
 	cd miniforge3
-	wget wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh 
+	curl -OL https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh 
 	mv Miniforge3-Linux-x86_64.sh miniforge3.sh
 	cd ..
 else
